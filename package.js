c0628807820ec3be7d96d48bb4373d290b7d5315
productiveme <jaco@productive.me>
2016-12-29 10:16:26 +0200

    Minor bugfixes

--- package.js
+++ package.js
@@ -1,10 +1,11 @@
 Package.describe({
-  name: 'alizbeo:flow-db-admin',
+  name: 'productiveme:flow-db-admin',
   version: '1.3.2',
   // Brief, one-line summary of the package.
+  shortDescription: 'Meteor Database Admin package for use with Flow Router',
   summary: 'Meteor Database Admin package for use with Flow Router',
   // URL to the Git repository containing the source code for this package.
-  git: 'https://github.com/JTL1992/alizbeo-flow-db-admin.git',
+  git: 'https://github.com:productiveme/alizbeo-flow-db-admin.git',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
